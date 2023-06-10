a90a05e6de43dded461dfc02a31a0b69bfdc9e01
ba144220 <hs41511@gmail.com>
2023-06-10 18:09:06 +0800

    [feat] landing page

--- oauth2_server/client/app/page.jsx
+++ oauth2_server/client/app/page.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <section className="w-full flex-center flex-col">
+    <section className="w-full h-screen pb-20 flex-center flex-col">
       <h1 className="head_text text-center">
-      Oauth 2.0 
+        Oauth 2.0
         <br className="max-md:hidden" />
         <span className="orange_gradient text-center"> with FIDO 2.0 </span>
       </h1>
@@ -12,8 +12,13 @@ const page = () => {
         {" "}
         Create your account and login with Fidoauth without passwords!
       </p>
+      <button className="mt-6 p-4 rounded-full bg-gradient-to-r from-amber-500 to-orange-500 hover:from-amber-400 hover:to-orange-400">
+        <span className="text-lg font-bold text-white font-montserrat ">
+          Create Account
+        </span>
+      </button>
     </section>
-  )
-}
+  );
+};
 
-export default page
+export default page;
