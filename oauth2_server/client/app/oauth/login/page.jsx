d248f6a3fb887568dd2bef558b32a079e8b75ca5
ba144220 <hs41511@gmail.com>
2023-06-14 21:51:21 +0800

    [feat] fix auth/login route

--- oauth2_server/client/app/oauth/login/page.jsx
+++ oauth2_server/client/app/oauth/login/page.jsx
@@ -1,8 +1,9 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { sendRequest } from "@utils/sendRequest";
+import { Login } from "@utils/webAuthn";
 
 const emptyUser = {
   personal_id: "",
@@ -14,10 +15,35 @@ const page = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      router.push("/profile");
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    let response = await sendRequest(
+      `/api/auth/id/${newUser.personal_id}`,
+      "GET"
+    );
+    if (!response?.ok) {
+      alert("Personal ID not exists!");
+      return;
+    }
+    const { cred_id } = await response.json();
+
+    const assertion = await Login(cred_id);
+
     setSubmitting(true);
-    const response = await sendRequest("/api/auth/login", "POST", newUser);
+    response = await sendRequest("/api/auth/login", "POST", {
+      personal_id: newUser.personal_id,
+      client_data: new Uint8Array(assertion.response.clientDataJSON),
+      auth_data: new Uint8Array(assertion.response.authenticatorData),
+      signature: new Uint8Array(assertion.response.signature),
+    });
     setSubmitting(false);
 
     if (response?.ok) {
@@ -26,6 +52,7 @@ const page = () => {
       router.push(
         `/oauth/authorize?client_id=${searchParams.get("client_id")}`
       );
+
       return;
     }
 
@@ -60,9 +87,6 @@ const page = () => {
         </div>
 
         <div className="flex-end mx-3 mb-5 gap-4">
-          <Link href="/" className="text-gray-500 text-sm">
-            Cancel
-          </Link>
           <button
             type="submit"
             disabled={submitting}
